21ceb09ecac841a8021123337978aae6eaadce7b
iecd <iecd@iedeimac-3.local>
2016-04-20 16:10:08 -0700

    Initial Commit

---
+++ 环信学习/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  环信学习
+//
+//  Created by iecd on 16/4/20.
+//  Copyright © 2016年 chenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 环信学习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  环信学习
+//
+//  Created by iecd on 16/4/20.
+//  Copyright © 2016年 chenjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
